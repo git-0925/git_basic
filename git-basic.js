e93a79f7cbf68ebd8c9cb30b6c0984766a35f595
M Hasan <hasanmuhammad197@gmail.com>
2025-09-11 18:49:53 +0700

    install & configuration

---
+++ git-basic.js
@@ -0,0 +1,17 @@
+//Git & VS Code
+https://git-scm.com/downloads
+https://code.visualstudio.com/
+
+//Konfigurasi utama : username & email
+git config --global user.name "M Hasan"
+git config --global user.email "hasanmuhammad197@gmail.com"
+
+//Set VS Code menjadi default editor untuk git
+git config --global core.editor "code --wait"
+
+//Set default diff tool
+git config --global diff.tool "default-difftool"
+git config --global difftool.default-difftool.cmd "code --wait --diff \$LOCAL \$REMOTE"
+
+//Show all configuration
+git config --list --show-origin
